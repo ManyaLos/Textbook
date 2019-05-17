61d77ed4e13c85a34bd149f0567344257ef9ac5f
ManyaLos <m.korovenkova@yandex.ru>
2019-05-17 23:54:43 +0400

    Initial commit

---
+++ src/main/java/com/epam/student_practice/textbook/Employee.java
@@ -0,0 +1,5 @@
+package main.java.com.epam.student_practice.textbook;
+
+public class Employee extends Human {
+    String managerName;
+}
---
+++ src/main/java/com/epam/student_practice/textbook/Human.java
@@ -0,0 +1,12 @@
+package main.java.com.epam.student_practice.textbook;
+
+public class Human {
+    String lastname;
+    String name;
+    int dateOfBirth;
+    String phoneNumber;
+
+public Human(){
+
+}
+}
